d349b8391e253e56913ec51218d02c66734eecd4
Chris Blake <telefragent@gmail.com>
2023-05-08 17:31:54 -0400

    Portraits

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -20,14 +20,14 @@ public class Fire : MonoBehaviour
 	{
 		for (int i = 0; i < lights.Length; i++)
 		{
-			lights[i].intensity = Mathf.Lerp(lights[i].intensity, lightTargets[i], Time.deltaTime);
+			lights[i].intensity = Mathf.Lerp(lights[i].intensity, lightTargets[i], Time.deltaTime * 5f);
 		}
 
-		if(Random.value > 0.99f)
+		if(Random.value > 0.95f)
 		{
 			for (int i = 0; i < lights.Length; i++)
 			{
-				lightTargets[i] = Random.Range(0.5f, 2f);
+				lightTargets[i] = Random.Range(0.5f, 1f);
 			}
 		}
 	}
